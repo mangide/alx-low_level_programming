6f262aca3f347c708e222ebef2afd30131db4df9
mangide <basileiazoe@gmail.com>
2023-03-25 06:23:30 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -38,3 +38,15 @@ void print_name_uppercase(char *name)
     }
 }
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_name("Bob", print_name_as_is);
+    print_name("Bob Dylan", print_name_uppercase);
+    printf("\n");
+    return (0);
+}
